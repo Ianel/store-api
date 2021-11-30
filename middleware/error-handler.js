2e51991591502e66bb0a8d3c03252b7e9f9fff67
Ianel Tombozafy <ianell.s118@gmail.com>
2021-11-30 13:35:08 +0300

    Customize error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,13 +1,15 @@
-const { CustomAPIError } = require('../errors');
+const { CustomAPIError, NotFoundError, BadRequestError, UnauthenticatedError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
+
+    if (err instanceof CustomAPIError || err instanceof NotFoundError || err instanceof BadRequestError || err instanceof UnauthenticatedError) {
         return res.status(err.statusCode).json({ msg: err.message });
     }
+
     return res
             .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ err });
+            .json({ msg: err.message });
 };
 
 module.exports = errorHandlerMiddleware;
